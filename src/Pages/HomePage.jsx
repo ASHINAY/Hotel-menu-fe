bf22c30f8a916e385c644ee92027daa6f43ab3f6
Nayak <anuja.nayak@capgemini.com>
2023-07-27 12:23:24 +0530

    appbar

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,41 +5,42 @@ import { useNavigate } from 'react-router-dom'
 function HomePage() {
   return (
     <div className='wholePage'>
-    <div className='MainContainer'>
+      <div className='MainContainer'>
         <div className='Title' >Hotel Anna Poorna</div>
-        <div>
-    <span>Tastes Like Heaven, Feels Like Home</span>
-    </div>
+
+        <span>Tastes Like Heaven, Feels Like Home</span>
+
+      </div>
+      <div className='Hotel'>Hotel Menu</div>
+      <div className='MorningContainer'>
+        <div className='MorningRow'>
+          <div className='Morning'>
+            <span>Morning Mood</span>
+          </div>
+
+          <div className='Morning'>
+            <span>Midday Munch</span>
+          </div>
+        </div>
+        <div className='MorningRow'>
+          <div className='Morning'>
+            <span>Lunch Luxuries</span>
+          </div>
+
+          <div className='Morning'>
+            <span>Sunset Sips</span>
+          </div>
+        </div>
+        <div className='lastBlock'>
+          <div className='Morning'>
+
+            <span>Moonlight stips</span>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <div className='Hotel'>Hotel Menu</div>
-    <div className='MorningContainer'>
-    <div className='Morning'>
-    <span style={{marginTop:"55px",fontSize:"10px",color:"#000000"
-,display:'flex',textAlign:'center'}}>Morning Mood</span>
-    </div>
 
-    <div className='Morning'>
-    <span style={{marginTop:"55px",fontSize:"10px",color:"#000000"
-,display:'flex',textAlign:'center'}}>Midday Munch</span>
-    </div>
-</div>
-    <div className='Morning'>
-    <span style={{marginTop:"55px",fontSize:"10px",color:"#000000"
-,display:'flex',textAlign:'center'}}>Lunch Luxuries</span>
-    </div>
-
-    <div className='Morning'>
-    <span style={{marginTop:"55px",fontSize:"10px",color:"#000000"
-,display:'flex',textAlign:'center'}}>Sunset Sips</span>
-    </div>
-
-    <div className='Morning'>
-    <span style={{marginTop:"55px",fontSize:"10px",color:"#000000"
-,display:'flex',textAlign:'center'}}>Moonlight stips</span>
-    </div>
-    </div>
-    
   )
 }
 
